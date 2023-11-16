475eaaf9166efd0a6ff76b90017f194406e8e2db
Ali Fraz <alifrazkhan92@gmail.com>
2023-11-16 12:35:58 +0000

    todo app final

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')
+    path('addtask/', views.addtask, name='addtask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_not_done/<int:pk>/', views.mark_as_not_done, name='mark_as_not_done'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+
 ]
